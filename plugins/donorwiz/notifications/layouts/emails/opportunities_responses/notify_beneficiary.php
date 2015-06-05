fe77aaac72289a959985b6dc3c00eae574f1bc44
yesinternet <dev.yesinternet@gmail.com>
2015-06-05 14:25:29 +0300

    New plugins & bug fixes
    - Contact form plugin
    - Added welcome email for new users
    - Fixed opportunity edot volunteers link in notify_beneficiary
    notification email

--- plugins/donorwiz/notifications/layouts/emails/opportunities_responses/notify_beneficiary.php
+++ plugins/donorwiz/notifications/layouts/emails/opportunities_responses/notify_beneficiary.php
@@ -24,10 +24,14 @@ $response_message=$displayData['response_message'];
 <div style="max-width:480px;margin:0 auto;padding:10px;">
 <h2 style="text-align:center;"><?php echo JText::_('PLG_DONORWIZ_NOTIFICATIONS_OPPORTUNITIES_RESPONSES_NEW_RESPONSE_NOTIFICATION_VOLUNTEERS_WROTE');?></h2>
 <p style="text-align:center;font-style: italic;">"<?php echo $response_message;?>"</p>
-<p style="text-align:center;"><?php echo JText::_('PLG_DONORWIZ_NOTIFICATIONS_OPPORTUNITIES_RESPONSES_NEW_RESPONSE_NOTIFICATION_LINK');?> <a target="_blank" href="<?php echo rtrim(JUri::base(), '/') . JRoute::_('index.php?option=com_donorwiz&view=dashboard&layout=dwopportunityvolunteers&Itemid=298&id='.$opportunity_id); ?>"><?php echo JText::_('PLG_DONORWIZ_NOTIFICATIONS_OPPORTUNITIES_RESPONSES_NEW_RESPONSE_NOTIFICATION_HERE');?></a></p>
+<p style="text-align:center;">
+    <?php echo JText::_('PLG_DONORWIZ_NOTIFICATIONS_OPPORTUNITIES_RESPONSES_NEW_RESPONSE_NOTIFICATION_LINK');?>
+    <a target="_blank" href="<?php echo JRoute::_('index.php?option=com_donorwiz&view=dashboard&layout=dwopportunityvolunteers&Itemid=298&id='.$opportunity_id , true , -1); ?>">
+        <?php echo JText::_('PLG_DONORWIZ_NOTIFICATIONS_OPPORTUNITIES_RESPONSES_NEW_RESPONSE_NOTIFICATION_HERE');?>
+    </a>
+</p>
 
 </div>
 </div>
 
 <?php echo JLayoutHelper::render( 'footer' , array() , JPATH_ROOT .'/components/com_donorwiz/layouts/mail/common' , null );?>
-
